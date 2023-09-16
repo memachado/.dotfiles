7a3f86a9f3b59f66c60ac51257edb6d7ae1838f2
Miguel <20932346+memachado@users.noreply.github.com>
2023-09-16 20:42:08 -0300

    New organization

---
+++ .aliases/general.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/zsh
+
+red='\033[31m'
+gre='\033[32m'
+blu='\033[36m'
+end='\e[0m'
+
+# General
+alias src='source ~/.zshrc'
+
+alias ls="exa --icons -b ${*}"
---
+++ .aliases/git.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/zsh
+
+# Git and Gerrit
+alias push="push"
+alias check="git checkout ${1}"
+alias cbm="git checkout master"
+alias cbd="git checkout develop"
+alias amend="git commit --amend"
+alias st='git status'
+alias sts='st -s'
+alias pull='git pull'
+alias add='add'
+alias tag='git tag'
+alias gmm='git merge master'
+alias gmd='git merge develop'
+alias gmc='git merge --continue'
+alias cmt="git commit -m ${*}"
+alias last='log'
+alias fet='git fetch'
+alias rst='git reset --soft HEAD~1'
+alias lcg='git log --all --graph --decorate --oneline --abbrev-commit'
+
+function add () {
+    if [[ -z ${1} ]]
+    then 
+        git add .
+    else
+        git add "${1}"
+    fi
+}
+
+function push () {
+    ramo=$(git branch | grep -oP '(?<=\* ).+')
+    git push origin HEAD:refs/for/${ramo}
+}
+
+function log () {
+    if [[ -z ${1} ]]
+    then 
+        git log -1
+    else
+        git log -"${1}"
+    fi
+}
