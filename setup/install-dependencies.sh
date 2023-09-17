a0893c658f13ad70dc967366e48dcfe110398f9a
Miguel <20932346+memachado@users.noreply.github.com>
2023-09-16 21:59:52 -0300

    Include dependencies install

---
+++ setup/install-dependencies.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+
+apt install vim -y
+apt install stow -y
+apt install exa -y
+apt install btop -y
+apt install ubuntu-restricted-extras
+apt install gnome-tweaks -y
+apt install synaptic -y
+apt install rclone -y
+apt install rclone-desktop -y
+apt install neofetch -y
