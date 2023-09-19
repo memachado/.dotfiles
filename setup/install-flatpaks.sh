969bdd3501589c845538d756abcbe4bbaedfb2bf
Miguel <20932346+memachado@users.noreply.github.com>
2023-09-19 20:19:38 -0300

    flatpak install

---
+++ setup/install-flatpaks.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+flatpak install com.heroicgameslauncher.hgl  -y
+flatpak install com.spotify.Client -y
+flatpak install io.dbeaver.DBeaverCommunity  -y
+flatpak install md.obsidian.Obsidian -y
+flatpak install net.lutris.Lutris  -y
+flatpak install org.gimp.GIMP  -y
+flatpak install org.gtk.Gtk3theme.Pop-dark -y
+flatpak install org.keepassxc.KeePassXC  -y
+flatpak install org.onlyoffice.desktopeditors  -y
+flatpak install org.qownnotes.QOwnNotes  -y
