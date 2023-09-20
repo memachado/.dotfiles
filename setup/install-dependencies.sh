d9254701fe84e4b57e5c5f6f6c5f2cf7da891680
Miguel <20932346+memachado@users.noreply.github.com>
2023-09-20 10:07:00 -0300

    fix install apps

--- setup/install-dependencies.sh
+++ setup/install-dependencies.sh
@@ -8,6 +8,8 @@ apt install btop -y
 apt install ubuntu-restricted-extras
 apt install gnome-tweaks -y
 apt install synaptic -y
+apt install vlc -y
+apt install rsync -y
 apt install rclone -y
 apt install rclone-desktop -y
 apt install neofetch -y
--- setup/install-flatpaks.sh
+++ setup/install-flatpaks.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/bash
 
+# games
 flatpak install com.heroicgameslauncher.hgl  -y
-flatpak install com.spotify.Client -y
-flatpak install io.dbeaver.DBeaverCommunity  -y
-flatpak install md.obsidian.Obsidian -y
 flatpak install net.lutris.Lutris  -y
-flatpak install org.gimp.GIMP  -y
-flatpak install org.gtk.Gtk3theme.Pop-dark -y
+
+#security
 flatpak install org.keepassxc.KeePassXC  -y
+
+# office
 flatpak install org.onlyoffice.desktopeditors  -y
 flatpak install org.qownnotes.QOwnNotes  -y
+
+# media
+flatpak install com.spotify.Client -y
+flatpak install org.gimp.GIMP  -y
+flatpak install md.obsidian.Obsidian -y
+
+# work
+flatpak install io.dbeaver.DBeaverCommunity  -y
+
+# extras
+flatpak install org.gtk.Gtk3theme.Pop-dark -y
