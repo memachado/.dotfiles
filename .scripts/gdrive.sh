25a01bea048e1df7e2f3bd78abe83fbfb5315544
Miguel <20932346+memachado@users.noreply.github.com>
2023-09-28 13:22:15 -0300

    clean a bit

--- .scripts/gdrive.sh
+++ .scripts/gdrive.sh
@@ -2,8 +2,5 @@
 
 # Verifique se a unidade está montada
 if ! mount | grep -q "GDrive"; then
-    # A unidade não está montada, então monte-a
     rclone mount google-drive: ~/GDrive --daemon
-else
-    echo "A unidade já está montada."
 fi
