8a7a2d5c0b2d120e8c147871ee76484dc504600d
Miguel <20932346+memachado@users.noreply.github.com>
2023-09-30 01:02:55 -0300

    remove zsh from dependencies files

--- setup/install-dependencies.sh
+++ setup/install-dependencies.sh
@@ -17,19 +17,3 @@ sudo add-apt-repository ppa:mmozeiko/rclone-browser
 
 apt install rclone-desktop -y
 apt install rclone -y
-
-# zsh
-apt install zsh
-
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/alias-tips
-
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
-
-# fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
