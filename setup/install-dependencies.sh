19887fe639c1b96edc21e904a976bc08a998bbca
Miguel <20932346+memachado@users.noreply.github.com>
2023-09-30 01:07:53 -0300

    include apt update and upgrade

--- setup/install-dependencies.sh
+++ setup/install-dependencies.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
 
+sudo add-apt-repository ppa:mmozeiko/rclone-browser
+
+apt update -y
+apt upgrade -y
 
 apt install vim -y
 apt install stow -y
 apt install exa -y
 apt install btop -y
-apt install ubuntu-restricted-extras
+apt install ubuntu-restricted-extras -y
 apt install gnome-tweaks -y
 apt install synaptic -y
 apt install vlc -y
@@ -13,7 +17,6 @@ apt install rsync -y
 apt install neofetch -y
 apt install dconf-cli -y
 
-sudo add-apt-repository ppa:mmozeiko/rclone-browser
 
 apt install rclone-desktop -y
 apt install rclone -y
