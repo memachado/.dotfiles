131cca725dbc687c5b8102f5235ef7edf4aa7c64
Miguel <20932346+memachado@users.noreply.github.com>
2023-09-30 01:31:38 -0300

    fix p10k

--- setup/install-oh-my-zsh.sh
+++ setup/install-oh-my-zsh.sh
@@ -7,7 +7,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/alias-tips
 
-git clone https://github.com/bhilburn/powerlevel9k.git $HOME/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 
 
 # fzf
