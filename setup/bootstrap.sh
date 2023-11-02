b320d2360dbff7cb47e4fb73cd08499954c19cb0
Miguel <20932346+memachado@users.noreply.github.com>
2023-11-02 03:00:51 -0300

    Block run as a root

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 
+# Check if running as root. If root, script will exit
+if [[ $EUID -eq 0 ]]; then
+    echo "This script should not be executed as root! Exiting..."
+    exit 1
+fi
+
 execute_if() {
     if [ -f "$1" ]; then
 
